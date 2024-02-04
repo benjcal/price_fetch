0de825ddeb5d4a743ab2080e44114a1e73244d48
Benjamin Calderon <benj.calderon@gmail.com>
2024-02-04 14:37:31 -0400

    alpaca api

---
+++ lib/alpaca.ex
@@ -0,0 +1,25 @@
+defmodule Alpaca do
+  alias Alpaca.Fetch
+  alias Alpaca.Cache
+
+  def get_symbol_price(symbol) do
+    cond do
+      Cache.exists?(symbol) ->
+        IO.puts("Cache hit")
+
+        Cache.get!(symbol)
+
+      true ->
+        IO.puts("API call")
+
+        case Fetch.latest_price(symbol) do
+          {:ok, price} ->
+            Cache.put!(symbol, price)
+            {:ok, price}
+
+          e ->
+            e
+        end
+    end
+  end
+end
